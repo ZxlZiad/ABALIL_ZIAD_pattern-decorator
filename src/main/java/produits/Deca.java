5b5cb6dea0e2b68d45ae0b797a2744ae83ddca01
abalil.ziad <abalil.ziad@gmail.com>
2023-12-10 10:30:24 +0100

    commit

---
+++ src/main/java/produits/Deca.java
@@ -0,0 +1,12 @@
+package produits;
+public class Deca extends Boisson {
+
+    public Deca() {
+        this.description="Deca";
+    }
+
+    @Override
+    public double cout() {
+        return 11.5;
+    }
+}
